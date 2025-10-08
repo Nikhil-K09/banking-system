7222880e76312dc8d17bc0c98777a8bdf0a9401a
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-10-08 13:04:47 +0530

    rest impl

--- src/main/java/com/example/banking/entity/Account.java
+++ src/main/java/com/example/banking/entity/Account.java
@@ -1,23 +1,22 @@
 package com.example.banking.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.aot.generate.GeneratedTypeReference;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="accounts")
+@Table(name = "accounts")
 @Entity
 public class Account {
     @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY) check reason depricate
     private Long id;
+
+    @Column(name = "account_holder_name")
     private String accountHolderName;
     private double balance;
 }
